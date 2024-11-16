456fcc2b3ca4a7630c9a0f8d01a0d50f50ec37a5
bugsssssss <n.soatov@bk.ru>
2024-11-16 19:33:13 +0500

    checking for secret key

--- webhook_listener.py
+++ webhook_listener.py
@@ -1,12 +1,29 @@
-from flask import Flask, request
+import hashlib
+import hmac
+from flask import Flask, abort, request
 import os
 import subprocess
 
 app = Flask(__name__)
 
 
+SECRET_KEY = "fvGL5puCNg7Lud4X"
+
+
+def verify_secret(request):
+    signature = request.headers.get("X-Hub-Signature-256")
+    if not signature:
+        abort(403)
+
+    _, signature = signature.split("=")
+    mac = hmac.new(SECRET_KEY.encode(), msg=request.data, digestmod=hashlib.sha1)
+    if not hmac.compare_digest(mac.hexdigest(), signature):
+        abort(403)
+
+
 @app.route("/webhook", methods=["POST"])
 def webhook_event():
+    verify_secret(request)
     if request.method == "POST":
         subprocess.run(["git", "pull"], cwd="/home/allplay/internetbor-ru-backend")
 
